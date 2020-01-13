83e6ca736dc805af0c0b606710b213fef6a74903
James <jamese1326@gmail.com>
2020-01-13 07:47:21 +0000

    Minor tidying

--- activity_EXPLORE.R
+++ activity_EXPLORE.R
@@ -5,25 +5,17 @@ library(tidyverse)
 library(lubridate)
 source("activity_FUNC.R")
 
-#log_master - the original data import
-#log - the original data with blank rows removed and some other cleaning
-#log_B, log_R, log_F - the data for types B,F,R with appropriate variables
-#log_B_split, log_F_split - B and F data with week data split up over following week
-#log_new - R combined with B and F splits. 9 variables so some running specific ones lost (paces, terrain, hs, strides)
-#totals - Daily totals for each of R,B,F (time, distance, ascent)
-#
-# 2018_clean.RDS is the main 2018 file
-log_2018 <- readRDS("data_processed/2018_clean.RDS")
-log_new <- readRDS("data_processed/log_new.RDS")
-#names(log_new)
+log18 <- readRDS("data_processed/log_2018.RDS")
+log13 <- readRDS("data_processed/log_2013.RDS")
+
 # Combine dfs. The older df uses "notes" for the activity name
-log_2018_simple <- log_2018 %>% 
-  select_at(c(names(log_new)[-1], "description")) %>%
+log18_simp <- log18 %>% 
+  select_at(c(names(log13)[-1], "description")) %>%
   select(-notes) %>% 
   rename(notes = description)
 
-log_all <- mutate(log_2018_simple, week_data = 0) %>%  
-  bind_rows(log_new) %>% 
+log_all <- mutate(log18_simp, week_data = 0) %>%  
+  bind_rows(log13) %>% 
   mutate(week_data = as.logical(week_data)) %>% 
   rename(name = notes)
 
@@ -41,13 +33,14 @@ log_new %>% group_by(type, week_data) %>% tally(distance)
 shoe_summary <- log_all %>% 
   filter(type == "R") %>% 
   group_by(subtype) %>%
-  summarise_at(funs(sum), .vars = c("time", "distance", "ascent")) %>%
+  summarise_at(.vars = c("time", "distance", "ascent"), sum) %>%
   arrange(desc(distance))
 shoe_summary
 
 #Annual volume
-volume <- log_all %>% filter(type %in% c("R", "B")) %>%
-  filter(year(date) <= 2019) %>% 
+volume <- log_all %>% 
+  filter(type %in% c("R", "B")) %>%
+  filter(year(date) <= 2019, year(date) > 2013) %>% 
   group_by(type, year = as.factor(year(date))) %>%
   summarise(distance = sum(distance),
             time = sum(time),
@@ -67,7 +60,19 @@ ggplot(volume, aes(x = year, y = time / 60 / 52, fill = type)) +
   geom_col() +
   ylab("weekly hours")
 
-# Five longest (distance) R, B and F activities.
+# Rides in 2016/17/18 ---------------
+log_all %>% 
+  mutate(year = year(date)) %>% 
+  filter(type == "B",
+         week_data == 0,
+         year %in% c(2016, 2017, 2018)) %>%
+  select(-week_data) %>% 
+  group_by(year) %>% 
+  top_n(n = 5, wt = distance) %>%
+  arrange(year, desc(distance))
+
+
+# Five longest (distance) R, B and F activities --------------
 log_all %>% filter(type %in% c("R", "B", "F")) %>%
   filter(week_data == 0) %>%
   select(-week_data) %>% 
