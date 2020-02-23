d23d4a77470444bc1df24c0705bb75a375afc9c8
James <jamese1326@gmail.com>
2020-02-23 22:12:00 +0000

    Add 2020 processing

---
+++ code_process/log_PROCESS_2020.R
@@ -0,0 +1,57 @@
+#############
+# Imports activity data from my 2020 spreadsheet. Activities are recorded on the "2020" sheet of the spreadsheet.
+# Main output is saved to "log_2020.RDS"
+#############
+
+library(tidyverse)
+library(readxl)
+library(edwards)
+
+file_name <- "C:/Users/James/Dropbox/Mine/Personal/Activity Record 2020.xlsx"
+
+log_master <- read_excel(file_name, sheet="2020", range = cell_cols("A:T"))
+
+# trim rows with no entry
+log <- filter(log_master, !is.na(Type))
+
+#Rename cols
+colnames(log) <- c("Date", "Type", "Subtype", "Time", "Distance", "Ascent", "Norm_power", "Description", "Terrain", 
+                   "Total_time", "Quick", "Feel", "Enjoy", "Physical_cost", "Mental_cost", 
+                   "Feel_after", "Quality", "Quality_types", "Time_on", "Notes")
+
+log <- mutate(log, Date=as.Date(Date)) #change Date to date object (from datetime)
+
+# Replace NAs
+log_2020 <- log %>%
+  mutate(Total_time = ifelse(is.na(Total_time), Time, Total_time)) %>%
+  mutate_if(is.numeric, ~replace_na(., 0)) %>%
+  replace_na(list(Subtype = "none")) %>% 
+  rename_all(str_to_lower)
+
+# Left NAs in Description,  Quality_types, Notes
+
+# Create new df with daily totals for each activity type (uses temp_df to add zero 
+# entries on days where there is no activity for a given type)
+# Uses temp df with zero entries for all days and types
+start <- min(log_2020$date)
+ndays <- as.numeric(max(log_2020$date) - start) + 1
+temp_df <- tibble(date = rep(start + (1 : ndays) - 1, 3), 
+                  type = rep(c("B", "F", "R"), 1, each=ndays),
+                  time = 0,
+                  distance = 0,
+                  ascent = 0)
+
+totals_2020 <- log_2020 %>% 
+  select(c(1:2, 4:6)) %>%
+  bind_rows(temp_df) %>% 
+  group_by(type, date) %>% 
+  summarise_all(sum) %>%
+  ungroup() %>% 
+  mutate(day=as.numeric(date - min(date) + 1)) %>%
+  arrange(date)
+
+if(F){
+  saveRDS(log_master, "data_processed/log_2020_raw.RDS")
+  saveRDS(log_2020, "data_processed/log_2020.RDS")
+  saveRDS(totals_2020, "data_processed/log_2020_totals.RDS")
+}
